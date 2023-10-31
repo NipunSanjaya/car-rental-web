e86104e42c2d091515e7fdeb5c81bc26e7639d66
chathushkasavinda <chathushkasavinda18@gmail.com>
2023-11-01 02:28:12 +0530

    content update user

--- BackEnd/src/main/java/lk/easy/car_rental/controller/ManageLoginController.java
+++ BackEnd/src/main/java/lk/easy/car_rental/controller/ManageLoginController.java
@@ -1,8 +1,10 @@
 package lk.easy.car_rental.controller;
 
 import lk.easy.car_rental.service.UserService;
+import lk.easy.car_rental.util.CurrentUserUtil;
 import lk.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,6 +17,14 @@ public class ManageLoginController {
     public ResponseUtil getUser(@RequestParam String username, @RequestParam String password) {
 
         return new ResponseUtil("OK", "Successfully Loaded..!", userService.getUser(username, password));
+
+    }
+
+    @GetMapping
+    public ResponseUtil getCurrentUserDetails() {
+        System.out.println(CurrentUserUtil.currentUser);
+
+        return new ResponseUtil("OK", "Successfully Loaded..!", CurrentUserUtil.currentUser);
 
     }
 }
