05849a8a5dccd22c3f3df475b1009a3329695216
chathushkasavinda <chathushkasavinda18@gmail.com>
2023-11-01 02:32:52 +0530

    content update admin

---
+++ BackEnd/src/main/java/lk/easy/car_rental/entity/Admin.java
@@ -0,0 +1,28 @@
+package lk.easy.car_rental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Admin {
+    @Id
+    private String nic;
+    private String name;
+    private String license;
+    private String address;
+    private String contact;
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+}
